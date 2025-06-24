3c28708d389319d67b42b80467c329be0e6a8ef3
Colland <t.jack.mac@gmail.com>
2025-06-24 14:17:20 +1200

    Finished game over screen + replay functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,42 @@ function App() {
   return (
     <div className="min-h-screen w-full flex flex-col">
       <header className="h-30 flex justify-center items-center">
-        <h1 className="text-[100px] font-['pokemonPixelFont'] font-semibold text-blue-50 ml-[70px]">
-          <span className="text-red-500">Poke</span> Dex
+        <h1 className="text-[100px] font-['pokemonPixelFont'] font-semibold text-blue-50 ml-[70px]"
+            style={{
+                    textShadow: `
+                        -2px -2px 0 #000,
+                        -2px -1px 0 #000,
+                        -2px  0px 0 #000,
+                        -2px  1px 0 #000,
+                        -2px  2px 0 #000,
+                        -1px -2px 0 #000,
+                        -1px -1px 0 #000,
+                        -1px  0px 0 #000,
+                        -1px  1px 0 #000,
+                        -1px  2px 0 #000,
+                         0px -2px 0 #000,
+                         0px -1px 0 #000,
+                         0px  1px 0 #000,
+                         0px  2px 0 #000,
+                         1px -2px 0 #000,
+                         1px -1px 0 #000,
+                         1px  0px 0 #000,
+                         1px  1px 0 #000,
+                         1px  2px 0 #000,
+                         2px -2px 0 #000,
+                         2px -1px 0 #000,
+                         2px  0px 0 #000,
+                         2px  1px 0 #000,
+                         2px  2px 0 #000
+                    `
+                }}
+          >
+            <span className="text-red-500">Poke</span> Dex
         </h1>
         <img src={pikachuImg} alt="pikachu" className="h-[4.2rem] mb-4 mr-3" />
       </header>
       
-      <main className="flex items-end">
+      <main className="flex items-end grow-1">
         <GameScreen />
       </main>
       
--- src/game/GameScreen.jsx
+++ src/game/GameScreen.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { shuffleArray } from "../utils";
+import { motion } from "framer-motion";
 import Card from "./Card";
 
 function GameScreen() {
     const [pokemonData, setPokemonData] = useState(null);
     const filteredPokemon = pokemonData ? filterPokemon(20) : null;
 
-    const [lives, setLives] = useState(3);
+    const hardCodedLives = 3;
+    const [lives, setLives] = useState(hardCodedLives);
     const [gameIsOver, setGameIsOver] = useState(false);
     const [score, setScore] = useState(0);
 
@@ -96,12 +98,24 @@ function GameScreen() {
     function gameOver() {
         setGameIsOver(true);
     }
-    
+
+    function resetGame() {
+        setLives(hardCodedLives);
+        setScore(0);
+        setGameIsOver(false);
+    }
 
     if(gameIsOver) {
         return (
-            <div>
-                <h1>GAME OVER BOY</h1>
+            <div className="flex flex-col justify-start items-center gap-4 w-96 h-48 p-4 m-auto rounded-lg bg-white border-4 border-black text-6xl font-[pokemonPixelFont] text-black">
+                <p>You lost!</p>
+                <motion.p
+                    animate={{ scale: 1.2 }}
+                    transition={{ repeat: Infinity, repeatType: "reverse", duration: 1}}
+                    onClick={resetGame}
+                >
+                    Try again?
+                </motion.p>
             </div>
         )
     }
@@ -109,11 +123,81 @@ function GameScreen() {
     return (
         <div>
             <div className="flex justify-center items-center gap-10 mt-[-20px]">
-                <p className="text-6xl text-white font-[pokemonPixelFont] rounded-xl pt-1 pb-1 pl-6 pr-6">Score: &nbsp;{score}</p>
-                <p className="text-6xl text-white font-[pokemonPixelFont] rounded-xl pt-1 pb-1 pl-6 pr-6">Lives: &nbsp;{lives}</p>
+                <motion.p
+                    key={`score-${score}`}
+                    animate={{ color: "#22e920", scale: 1.2 }}
+                    transition={{ repeat: 1, repeatType: "reverse", duration: 0.2 }}
+                    className="text-6xl text-white font-[pokemonPixelFont] rounded-xl pt-1 pb-1 pl-6 pr-6"
+                    style={{
+                        textShadow: `
+                            -2px -2px 0 #000,
+                            -2px -1px 0 #000,
+                            -2px  0px 0 #000,
+                            -2px  1px 0 #000,
+                            -2px  2px 0 #000,
+                            -1px -2px 0 #000,
+                            -1px -1px 0 #000,
+                            -1px  0px 0 #000,
+                            -1px  1px 0 #000,
+                            -1px  2px 0 #000,
+                            0px -2px 0 #000,
+                            0px -1px 0 #000,
+                            0px  1px 0 #000,
+                            0px  2px 0 #000,
+                            1px -2px 0 #000,
+                            1px -1px 0 #000,
+                            1px  0px 0 #000,
+                            1px  1px 0 #000,
+                            1px  2px 0 #000,
+                            2px -2px 0 #000,
+                            2px -1px 0 #000,
+                            2px  0px 0 #000,
+                            2px  1px 0 #000,
+                            2px  2px 0 #000
+                        `
+                    }}
+                >
+                    Score: &nbsp;{score}
+                </motion.p>
+                <motion.p
+                    key={`lives-${lives}`}
+                    animate={{ color: "#fb2c36", scale: 1.2 }}
+                    transition={{ repeat: 1, repeatType: "reverse", duration: 0.2 }}
+                    className="text-6xl text-white font-[pokemonPixelFont] rounded-xl pt-1 pb-1 pl-6 pr-6"
+                    style={{
+                        textShadow: `
+                            -2px -2px 0 #000,
+                            -2px -1px 0 #000,
+                            -2px  0px 0 #000,
+                            -2px  1px 0 #000,
+                            -2px  2px 0 #000,
+                            -1px -2px 0 #000,
+                            -1px -1px 0 #000,
+                            -1px  0px 0 #000,
+                            -1px  1px 0 #000,
+                            -1px  2px 0 #000,
+                            0px -2px 0 #000,
+                            0px -1px 0 #000,
+                            0px  1px 0 #000,
+                            0px  2px 0 #000,
+                            1px -2px 0 #000,
+                            1px -1px 0 #000,
+                            1px  0px 0 #000,
+                            1px  1px 0 #000,
+                            1px  2px 0 #000,
+                            2px -2px 0 #000,
+                            2px -1px 0 #000,
+                            2px  0px 0 #000,
+                            2px  1px 0 #000,
+                            2px  2px 0 #000
+                        `
+                    }}
+                >
+                        Lives: &nbsp;{lives}
+                </motion.p>
             </div>
 
-            <div className="flex justify-center flex-wrap gap-10 p-40">
+            <div className="flex justify-center flex-wrap gap-10 p-42">
                 {pokemonData ? filteredPokemon.map((pokemon) => <Card pokemon={pokemon} cardClicked={cardClicked} key={pokemon.id} />) : "Loading..."}
             </div>
         </div>
